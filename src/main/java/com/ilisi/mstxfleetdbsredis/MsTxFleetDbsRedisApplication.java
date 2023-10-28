46ccdfadb5c1467028618ed3897c5acafbee278f
musdev <w0l1d.dev@gmail.com>
2023-10-28 23:50:42 +0100

    init commit

---
+++ src/main/java/com/ilisi/mstxfleetdbsredis/MsTxFleetDbsRedisApplication.java
@@ -0,0 +1,13 @@
+package com.ilisi.mstxfleetdbsredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTxFleetDbsRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTxFleetDbsRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilisi/mstxfleetdbsredis/MsTxFleetDbsRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilisi.mstxfleetdbsredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTxFleetDbsRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
