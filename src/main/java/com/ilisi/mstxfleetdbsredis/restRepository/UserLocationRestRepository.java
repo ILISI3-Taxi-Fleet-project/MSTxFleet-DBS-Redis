d6557c9165d17a53d86a981f2379c5d388dbd21f
musdev <w0l1d.dev@gmail.com>
2023-12-11 00:08:44 +0100

    #13 add rest repo annotation

--- src/main/java/com/ilisi/mstxfleetdbsredis/restRepository/UserLocationRestRepository.java
+++ src/main/java/com/ilisi/mstxfleetdbsredis/restRepository/UserLocationRestRepository.java
@@ -2,11 +2,12 @@ package com.ilisi.mstxfleetdbsredis.restRepository;
 
 import com.ilisi.mstxfleetdbsredis.model.UserLocation;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(collectionResourceRel = "userLocation", path = "userLocation")
 public interface UserLocationRestRepository extends CrudRepository<UserLocation, String> {
 
     Optional<UserLocation> findByUserId(String userId);
-
 }
